746de98de23adb6b41e8a9cdd24828fd85b3ab1e
ayeonChoi <201120cay@dsm.hs.kr>
2022-05-18 09:46:37 +0900

    jwt 파일 추가

---
+++ src/jwt/passport/jwt.strategy.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { InjectRepository } from "@nestjs/typeorm";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { badRequestException } from "src/exception/exception.index";
+import { Teacher } from "src/teacher/entities/teacher.entity";
+import { TeacherRepository } from "src/teacher/entities/teacher.repository";
+import { Payload } from "../payload/jwt.payload";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(Teacher)
+    private readonly teacherRepository: TeacherRepository
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET_KEY,
+      ignoreExpiration: false,
+    });
+  }
+
+  async validate(payload: Payload) {
+    const teacher = await this.teacherRepository.findOne({
+      where: { teacherId: payload.teacherId },
+    });
+
+    if (teacher) {
+      return teacher;
+    } else {
+      throw badRequestException;
+    }
+  }
+}
