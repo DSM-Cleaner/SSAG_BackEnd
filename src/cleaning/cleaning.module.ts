87ecff19f1dd1f68b21d8368a734d416988de67d
ayeonChoi <201120cay@dsm.hs.kr>
2022-05-30 09:08:46 +0900

    getWeekRooms  추가 (#15)

--- src/cleaning/cleaning.module.ts
+++ src/cleaning/cleaning.module.ts
@@ -1,6 +1,8 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { RoomCleaningRepository } from "src/room-cleaning/entities/room-cleaning.repository";
 import { RoomCleaningModule } from "src/room-cleaning/room-cleaning.module";
+import { RoomRepository } from "src/room/entities/room.repository";
 import { RoomModule } from "src/room/room.module";
 import { CleaningController } from "./cleaning.controller";
 import { CleaningService } from "./cleaning.service";
@@ -8,7 +10,11 @@ import { CleaningRepository } from "./entities/cleaning.repository";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([CleaningRepository]),
+    TypeOrmModule.forFeature([
+      CleaningRepository,
+      RoomRepository,
+      RoomCleaningRepository,
+    ]),
     RoomCleaningModule,
     RoomModule,
   ],
